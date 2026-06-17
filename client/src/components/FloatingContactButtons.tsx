2d3d5f61d8b2cec23a8b6c07d33dd9671aa9e056
babu <babu@kullibabu.com>
2026-06-17 16:14:36 +0530

    Phone Number is Updated

--- client/src/components/FloatingContactButtons.tsx
+++ client/src/components/FloatingContactButtons.tsx
@@ -2,7 +2,7 @@ import { Phone} from "lucide-react";
 import { FaWhatsapp } from "react-icons/fa";
 
 const FloatingContactButtons = () => {
-  const phoneNumber = "9948740733";
+  const phoneNumber = "8019929888";
 
   return (
     <div className="fixed bottom-6 right-6 z-50 flex flex-col gap-3">
